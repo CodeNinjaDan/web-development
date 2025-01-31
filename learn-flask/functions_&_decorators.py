1c6d10f55aebe7df7165b11faec83bad7f60400e
CodeNinjaDan <dnlmuiru@gmail.com>
2025-01-31 22:44:43 +0300

    more flask & function features

--- learn-flask/functions_&_decorators.py
+++ learn-flask/functions_&_decorators.py
@@ -70,3 +70,60 @@ def say_greeting():
     print("How are you?")
 decorated_function = delay_decorator(say_greeting)
 decorated_function()
+
+
+# --------------------------------------- Function Composition (Combining Functions) -----------------------------------
+def combine_functions(func1, func2):
+    def composed_function(*args, **kwargs):  # Handle arguments generically
+        func1(*args, **kwargs)
+        func2(*args, **kwargs)
+    return composed_function
+
+composed_func = combine_functions(func1, func2)  # Combine func1 and func2
+
+@my_decorator  # Apply the decorator to the composed function
+def my_combined_function():
+    composed_func() # Call the combined function
+
+my_combined_function()
+
+
+# --------------------------------------------- Passing Arguments to Decorators ----------------------------------------
+def my_decorator(message):  # Decorator takes an argument
+    def decorator(func):  # Inner decorator function
+        def wrapper():
+            print(message)  # Use the argument
+            func()
+        return wrapper
+    return decorator
+
+@my_decorator("Applying to func1")
+def func1():
+    print("Function 1")
+
+@my_decorator("Applying to func2")
+def func2():
+    print("Function 2")
+
+func1()
+func2()
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in:
+            function(args[0])
+    return wrapper
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+new_user = User("angela")
+new_user.is_logged_in = True
+create_blog_post(new_user)
--- learn-flask/learn.py
+++ learn-flask/learn.py
@@ -2,14 +2,55 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Line 6 defines the content of the home page
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+# Content of the home page:
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return ('<h1 style="text-align: center">Hello, World!</h1>'
+            '<p><h2>I like this car</h2></p>'
+            '<img src="https://imgs.search.brave.com/QvMgZuOTozi_D3Xx7CP04bXXM6efuoZSlQwu04LvjKI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Z29vZHdvb2QuY29t/L2dsb2JhbGFzc2V0/cy8ucm9hZC0tcmFj/aW5nL2YxLzIwMjQv/LS0tY2FyLWxhdW5j/aGVzLWFuZC1saXZl/cmllcy9mZXJyYXJp/L2ZlcnJhcmktcmV2/ZWFscy1pdHMtMjAy/NC1mMS1jYXItMDIu/anBnP3J4eT0wLjUs/MC41JndpZHRoPTY0/MCZoZWlnaHQ9MzYw">')
+
+@app.route("/bye")
+def bye():
+    return "Bye!"
+
+# Create a variable
+@app.route("/username/<name>/1")
+def greet(name):
+    return f'<h1 style="text-align: center">Hello there {name}</h1>'
+
+# Variable as a Path
+@app.route("/username/<path:test>")
+@make_bold
+def as_path(test):
+    return f"This is the path {test}"
+
+# Multiple Variables
+@app.route("/test/<name>/<int:number>")
+@make_emphasis
+def years(name, number):
+    return f"Hello there {name}, you are {number} years old!"
+
+
 
 # This tells Flask that you're not running code from an imported module
 # It also makes it easier to run flask apps since you don't have to use export FLASK_APP=...
 # to run flask apps and ctrl c end the apps
 
 if __name__ == "__main__":
+    # Set debug mode to True to allow auto-refresh when you make changes
     app.run(debug=True)
