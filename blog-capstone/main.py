48e7db2affd983400fe6e84bd043b9fd9dc53236
CodeNinjaDan <dnlmuiru@gmail.com>
2025-02-04 12:17:04 +0300

    complete blog post page

--- blog-capstone/main.py
+++ blog-capstone/main.py
@@ -1,22 +1,28 @@
 from flask import Flask, render_template
+from post import Post
 import requests
 
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    blogs_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    all_blogs = blogs_response.json()
-
-    return render_template("index.html", posts=all_blogs)
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
 
 
-@app.route('/posts/<num>')
-def get_blog(num):
-    if num == 1:
-
-    return render_template("post.html", num=num)
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
