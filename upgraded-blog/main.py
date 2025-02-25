cbf3b6421b0c5a365a67b29b33c31f5d20e889da
CodeNinjaDan <dnlmuiru@gmail.com>
2025-02-25 23:59:32 +0300

    render each post

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -3,10 +3,11 @@ import requests
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/5b776c78600bd91b0e9a").json()
+
 
 @app.route("/")
 def home():
-    posts = requests.get("https://api.npoint.io/5b776c78600bd91b0e9a").json()
     return render_template("index.html", posts=posts)
 
 @app.route("/about")
@@ -17,7 +18,13 @@ def about():
 def contact():
     return render_template("contact.html")
 
-
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
