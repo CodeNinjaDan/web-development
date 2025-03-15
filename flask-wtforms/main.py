36b1d20f1e4d04abc1454aa4d1bb61a167c03f5d
CodeNinjaDan <dnlmuiru@gmail.com>
2025-03-15 23:28:56 +0300

    added validation & fixes

--- flask-wtforms/main.py
+++ flask-wtforms/main.py
@@ -1,12 +1,14 @@
 import os
-
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email(message="Invalid email address")])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8, message="Password must be at least 8 characters long")])
+    submit = SubmitField(label='Log In')
 
 app = Flask(__name__)
 app.secret_key = os.getenv("APP_SECRET_KEY")
@@ -16,9 +18,15 @@ app.secret_key = os.getenv("APP_SECRET_KEY")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        #Process data if form is valid
+        if login_form.email.data == "admin@email.com":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=login_form)
 
 
