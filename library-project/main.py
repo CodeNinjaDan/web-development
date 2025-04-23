f73bea4b8d1c747a2523717000074d053ac6adbb
CodeNinjaDan <dnlmuiru@gmail.com>
2025-04-23 23:26:22 +0300

    added sqlite

--- library-project/main.py
+++ library-project/main.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+db.init_app(app)
 
-all_books = []
 
+class Books(db.Model):
+    id: Mapped[int] = mapped_column(Integer, unique=True, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
+    result = db.session.execute(db.select(Books).order_by(Books.title))
+    all_books = result.scalars()
     return render_template('index.html', books=all_books)
 
 
@@ -18,7 +37,10 @@ def add():
             "author": request.form["author"],
             "rating": request.form["rating"]
         }
-        all_books.append(new_book)
+        add_book = Books(**new_book)
+        db.session.add(add_book)
+        db.session.commit()
+
         return redirect(url_for('home'))
 
     return render_template('add.html')
