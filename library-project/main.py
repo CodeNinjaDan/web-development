470782c6dddd6f40fcba7106bef3269456db4ae8
CodeNinjaDan <dnlmuiru@gmail.com>
2025-04-23 23:35:05 +0300

    added update and delete db features

--- library-project/main.py
+++ library-project/main.py
@@ -46,6 +46,32 @@ def add():
     return render_template('add.html')
 
 
+@app.route('/edit/<int:id>', methods=["GET", "POST"])
+def edit(id):
+    # Fetch book by primary key
+    book_to_update = db.get_or_404(Books, id)
+
+    if request.method == "POST":
+        new_rating = request.form["rating"]
+        book_to_update.rating = new_rating
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template('edit_rating.html', book_to_update=book_to_update)
+
+
+@app.route('/delete')
+def delete():
+    book_id = request.args.get('id')
+
+    book_to_delete = db.get_or_404(Books, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
